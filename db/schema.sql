1cfdc6a374d557de1c8181ba5fe2006ff8305e9d
Alyssa Santopadre <alyssasantopadre@Alyssas-MacBook-Pro.local>
2017-05-06 10:42:17 -0400

    Adding the DB

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE errands_db;
+USE errands_db;
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+	user_name VARCHAR(70) NOT NULL,
+	user_email VARCHAR(70) NOT NULL,
+	user_password VARCHAR(40) NULL,
+	user_token VARCHAR(200) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE tasks (
+	id INT NOT NULL AUTO_INCREMENT,
+	keyword VARCHAR(100) NOT NULL,
+	task VARCHAR(100) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE todo (
+	id INT NOT NULL AUTO_INCREMENT,
+	weekday VARCHAR(70) NOT NULL,
+	user_id VARCHAR(70) NOT NULL,
+	task_id VARCHAR(40) NULL,
+	start_time VARCHAR(20) NOT NULL,
+	duration VARCHAR(80) NOT NULL,
+	PRIMARY KEY(id)
+);
