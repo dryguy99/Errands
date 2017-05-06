bb004eb721cb98d10d70d058b9f9ceffb50e30bf
Alyssa Santopadre <alyssasantopadre@Alyssas-MacBook-Pro.local>
2017-05-06 12:59:53 -0400

    API Routes

---
+++ models/todo.js
@@ -0,0 +1,27 @@
+
+module.exports = function (sequelize, DataTypes) {
+	var todos = sequelize.define("todos",{
+		todos_task: {
+			type: DataTypes.STRING,
+			validate: { 
+				len: [1] }
+		}
+	},
+
+		{
+		
+	      	// We're saying that we want our User to have To do tasks
+	  		classMethods: {
+	    		associate: function(models) {
+	          		// A user (foreignKey) is required or a todo task can't be made
+	          		todo.belongsTo(models.users, {
+	            		foreignKey: {
+	              		allowNull: false
+	            		}
+	          		})
+	    		}
+	  		}
+  		}
+    );
+	return todos;
+};
---
+++ routes/todo-api-routes.js
@@ -0,0 +1,77 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the posts
+  app.get("/api/todo", function(req, res) {
+    var query = {};
+    if (req.query.users_id) {
+      query.usersId = req.query.usesr_id;
+    }
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.todos.findAll({
+      where: query,
+      include: [db.users]
+    }).then(function(dbtodos) {
+      res.json(dbtodos);
+    });
+  });
+
+  // Get rotue for retrieving a single post
+  app.get("/api/todo/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.todos.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.users]
+    }).then(function(dbtodos) {
+      res.json(dbtodos);
+    });
+  });
+
+  // POST route for saving a new post
+  app.post("/api/todo", function(req, res) {
+    db.todos.create(req.body).then(function(dbtodos) {
+      res.json(dbtodos);
+    });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/todo/:id", function(req, res) {
+    db.todos.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbtodos) {
+      res.json(dbtodos);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/todo", function(req, res) {
+    db.todos.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbtodos) {
+        res.json(dbtodos);
+      });
+  });
+};
