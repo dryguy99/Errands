3496bb913e8a390a70f1f05c9f0c40f53aed246b
Alyssa Santopadre <alyssasantopadre@Alyssas-MacBook-Pro.local>
2017-05-08 19:46:32 -0400

    Tasks Table

---
+++ routes/task-api-routes.js
@@ -0,0 +1,45 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/tasks", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.tasks.findAll({
+      include: [db.todos]
+    }).then(function(dbtasks) {
+      res.json(dbtasks);
+    });
+  });
+
+  app.get("/api/tasks/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.tasks.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.todos]
+    }).then(function(dbtasks) {
+      res.json(dbtasks);
+    });
+  });
+
+  app.post("/api/tasks", function(req, res) {
+    db.tasks.create(req.body).then(function(dbtasks) {
+      res.json(dbtasks);
+    });
+  });
+
+  app.delete("/api/tasks/:id", function(req, res) {
+    db.tasks.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbtasks) {
+      res.json(dbtasks);
+    });
+  });
+
+};
