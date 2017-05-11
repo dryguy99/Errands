46cb80ebca0b211c0942e43c7280059a11de31bc
Alyssa Santopadre <alyssasantopadre@Alyssas-MacBook-Pro.local>
2017-05-10 22:23:17 -0400

    update

--- models/users_table.js
+++ models/users_table.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
       validate: {len: [1] }
       },
     phonenumber: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false,
       validate: {len: [1] }
       }
