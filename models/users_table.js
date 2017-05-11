25af8422f5dedca8d9492a128c9c6bee0ed59a27
Alyssa Santopadre <alyssasantopadre@Alyssas-MacBook-Pro.local>
2017-05-10 20:34:20 -0400

    table update

--- models/users_table.js
+++ models/users_table.js
@@ -1,14 +1,9 @@
-var  bcrypt = require('bcrypt-nodejs');
-
 module.exports = function(sequelize, DataTypes) {
   var users = sequelize.define("users", {
     // Giving the Author model a name of type STRING
     name: {
       type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        len: [2,140]
-        }
+      validate: {len: [2,140] }
       },
     email: {
       type: DataTypes.STRING,
@@ -16,12 +11,13 @@ module.exports = function(sequelize, DataTypes) {
       },
     password: {
       type: DataTypes.STRING,
-      validate: {len: [4] }
+      validate: {len: [1] }
       },
     token: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {len: [1] }
-      }
+      },
   },
     // Here we'll pass a second "classMethods" object into the define method
     // This is for any additional configuration we want to give our models
@@ -34,34 +30,9 @@ module.exports = function(sequelize, DataTypes) {
           users.hasMany(models.todos, {
             onDelete: "cascade"
           });
-        },
-        validPassword: function (password, passwd, done, user) {
-          bcrypt.compare(password, passwd, function (err, isMatch) {
-            if (err) {console.log(err);}
-            if (isMatch) {
-              return done(null, user);
-            } else {
-              return done(null, false);
-            }
-          });
         }
       }
-    },
-    {
-      dialect: "mysql"
     }
   );
-
-  users.beforeCreate(function (user, options, fn) {
-    var salt = bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-      return salt;
-    });
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      console.log(fn);
-      return fn(null, user);
-    });
-  });
   return users;
 };
